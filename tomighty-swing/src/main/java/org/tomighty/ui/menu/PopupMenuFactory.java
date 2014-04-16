fb3c695a530956c7384ae00cea53b198a446fa6f
Piotr Sobczyk <piotr.sobczyk@nsn.com>
2014-04-16 20:00:32 +0200

    Picking projects from side menu

--- tomighty-swing/src/main/java/org/tomighty/ui/menu/PopupMenuFactory.java
+++ tomighty-swing/src/main/java/org/tomighty/ui/menu/PopupMenuFactory.java
@@ -4,23 +4,38 @@ import java.awt.event.ActionListener;
 
 import javax.inject.Inject;
 import javax.swing.Action;
+import javax.swing.ButtonGroup;
 import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
+import javax.swing.JRadioButtonMenuItem;
 
+import org.tomighty.config.Projects;
 import org.tomighty.i18n.Messages;
 
 import com.google.inject.Injector;
 
 public class PopupMenuFactory {
 
-    @Inject private Injector injector;
-    @Inject private Messages messages;
+    @Inject
+    private Injector injector;
+    @Inject
+    private Messages messages;
+    @Inject
+    private Projects projects;
 
     public JPopupMenu create(Action[] items) {
         JPopupMenu menu = new JPopupMenu();
 
-        if(items != null && items.length > 0) {
-            for(Action action : items) {
+        ButtonGroup projectsGroup = new ButtonGroup();
+        for (String project : projects.getProjects()) {
+            JMenuItem item = new JRadioButtonMenuItem(project);
+            projectsGroup.add(item);
+            menu.add(item);
+        }
+        menu.addSeparator();
+
+        if (items != null && items.length > 0) {
+            for (Action action : items) {
                 injector.injectMembers(action);
                 JMenuItem item = new JMenuItem(action);
                 menu.add(item);
